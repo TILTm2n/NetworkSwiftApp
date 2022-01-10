e1ee083361b5e1f1612d6d830ebf008da3a7a46e
Eugene <evgen29031999@bk.ru>
2022-01-10 23:17:26 +0300

    добавление правила encoding в объект

--- NetworkApp/Post.swift
+++ NetworkApp/Post.swift
@@ -7,9 +7,18 @@
 
 import Foundation
 
-struct Post{
+//протокол Codable позволяет кодировать(Encoding) и декодировать(Decoding) данные
+struct Post: Codable{
     let userId: Int
-    let id: Int
+    let postId: Int
     let title: String
     let body: String
+    
+    enum CodingKeys: String, CodingKey{
+        case userId
+        //если полу в модели не совпадает с полем в JSON-объекте, то можно написать так
+        case postId = "id"
+        case title
+        case body
+    }
 }
