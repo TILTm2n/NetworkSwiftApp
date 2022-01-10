773e8e71591288e18f5a4892b938778b5f7b14d8
Eugene <evgen29031999@bk.ru>
2022-01-10 23:59:59 +0300

    add JSON decoder

--- NetworkApp/ViewController.swift
+++ NetworkApp/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     //конфигурация сесси по умолчанию
     let sessionConfiguration = URLSessionConfiguration.default
-    
+    let decoder = JSONDecoder()
     let session = URLSession.shared
     
     override func viewDidLoad() {
@@ -26,8 +26,16 @@ class ViewController: UIViewController {
         
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {return}
         
-        session.dataTask(with: url) { (data, response, error) in
+        session.dataTask(with: url) { [weak self] (data, response, error) in
             
+            guard let stringSelf = self else {return}
+            if error == nil, let parseData = data {
+                
+                let posts = try? stringSelf.decoder.decode([Post].self, from: parseData)
+                print("Posts: \(posts?.count)")
+            }else{
+                print("\(error)")
+            }
             
         }.resume()
     }
