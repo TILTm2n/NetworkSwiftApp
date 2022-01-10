c6950f725c25e3df3eb958e035db007c17a3e155
Eugene <evgen29031999@bk.ru>
2022-01-10 22:35:19 +0300

    create struct Post

---
+++ NetworkApp/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  NetworkApp
+//
+//  Created by Eugene on 10.01.2022.
+//
+
+import Foundation
+
+struct Post{
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
